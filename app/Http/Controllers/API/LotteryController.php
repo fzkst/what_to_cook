7fd259090e3829f0a321a09be2155a5582496c6e
Fazekas Tamás <tamas.fzks@gmail.com>
2025-03-15 17:24:46 +0100

    LotteryController: getFile(), skandi(), hatos(), eurojackpot()

--- app/Http/Controllers/API/LotteryController.php
+++ app/Http/Controllers/API/LotteryController.php
@@ -5,35 +5,64 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use PhpParser\Node\Expr\Cast\String_;
 
 class LotteryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function otos() {
-        
-        $url = 'https://bet.szerencsejatek.hu/cmsfiles/otos.csv';
-
+    public function getFile(String $url) {
         $response = Http::get($url);
-
         if ($response->failed()) {
             return response()->json(['error' => 'Nem sikerült letölteni a fájlt'], 500);
         }
-
         $content = $response->body();
+        return $content;
+    }
+
+    public function otos() {
+        $url = 'https://bet.szerencsejatek.hu/cmsfiles/otos.csv';
+        $content = $this->getFile($url);
 
         return response()->streamDownload(function () use ($content) {
             echo $content;
         }, 'otos.csv', [
             'Content-Type' => 'text/csv',
         ]);
-
     }
 
-    public function skandi()
-    {
-        //
+    public function skandi() {
+        $url = 'https://bet.szerencsejatek.hu/cmsfiles/skandi.csv';
+        $content = $this->getFile($url);
+
+        return response()->streamDownload(function () use ($content) {
+            echo $content;
+        }, 'skandi.csv', [
+            'Content-Type' => 'text/csv',
+        ]);
+    }
+
+    public function hatos(){
+        $url = 'https://bet.szerencsejatek.hu/cmsfiles/hatos.csv';
+        $content = $this->getFile($url);
+
+        return response()->streamDownload(function () use ($content) {
+            echo $content;
+        }, 'hatos.csv', [
+            'Content-Type' => 'text/csv',
+        ]);
+    }
+
+    public function eurojackpot(){
+        $url = 'https://bet.szerencsejatek.hu/cmsfiles/eurojackpot.csv';
+        $content = $this->getFile($url);
+
+        return response()->streamDownload(function () use ($content) {
+            echo $content;
+        }, 'eurojackpot.csv', [
+            'Content-Type' => 'text/csv',
+        ]);
     }
 
 }
